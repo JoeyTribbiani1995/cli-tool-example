9f992c8c6d555b6e0edbf8d1aee3669080efdd37
Dũng Võ <dungvph@fpt.com.vn>
2022-01-13 15:24:55 +0700

    commit

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 	"math/rand"
 	"regexp"
 	"strconv"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	dice := flag.String("d", "d6", "The type of dice to roll. Format: dX where X is an integer Default: d6")
+	numRoll := flag.Int("n", 1, "The number of die to roll. Default: 1")
 	flag.Parse()
 
 	matched, _ := regexp.Match("d\\d+", []byte(*dice))
@@ -21,10 +24,11 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		for i := 0; i < *numRoll; i++ {
+			roll := rand.Intn(d) + 1
+			fmt.Printf("You rolled a %d\n", roll)
+		}
 
-		roll := rand.Intn(d) + 1
-
-		fmt.Println(roll)
 	} else {
 		log.Fatalf("Improper formt for dice. Format should be dX where X is integer")
 	}
